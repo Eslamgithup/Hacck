8a9cc4804c81418247930e8e9fcce808ddd9c2d0
Eslamgithup <eslamshabaan2020@gmail.com>
2025-01-12 02:14:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿// إظهار نافذة إدخال رقم الهاتف
+function showPhoneNumberPopup() {
+    document.getElementById("phone-popup").style.display = "flex";
+}
+
+// عرض بيانات الحساب بعد إدخال رقم الهاتف
+function submitPhoneNumber() {
+    const phoneNumber = document.getElementById("mobile-number").value;
+    if (phoneNumber.trim() === "") {
+        alert("يرجى إدخال رقم الموبايل.");
+    } else {
+        document.getElementById("phone-popup").style.display = "none";
+        document.getElementById("account-info").style.display = "block";
+        document.getElementById("user-phone").textContent = phoneNumber;
+    }
+}
